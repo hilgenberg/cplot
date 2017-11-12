82385428ae53d3eeda63d398a0ceeb0f2f20acec
Torsten Hilgenberg <th@zoon.cc>
2017-11-12 21:20:08 +0100

    set exe file version

--- Windows/res/Resource.h
+++ Windows/res/Resource.h
@@ -6,32 +6,13 @@
 #define IDD_ABOUTBOX                    100
 #define IDD_SIDEVIEW                    101
 #define IDR_POPUP_EDIT                  119
-#define ID_STATUSBAR_PANE1              120
-#define ID_STATUSBAR_PANE2              121
-#define IDS_STATUS_PANE1                122
-#define IDS_STATUS_PANE2                123
-#define IDS_TOOLBAR_STANDARD            124
-#define IDS_TOOLBAR_CUSTOMIZE           125
-#define ID_VIEW_CUSTOMIZE               126
 #define IDR_MAINFRAME                   128
-#define IDR_MAINFRAME_256               129
 #define IDR_CPlotTYPE                   130
 #define IDS_EDIT_MENU                   306
-#define IDD_DIALOG1                     310
 #define IDD_PARAMETER                   310
 #define IDD_MAINVIEW                    311
 #define IDD_DEFINITION                  312
-#define IDD_DIALOG2                     317
-#define IDD_SIDEVIEWPARAM               317
-#define IDC_BUTTON1                     1000
-#define IDC_MFCBUTTON1                  1001
-#define IDC_CHECK1                      1002
 #define IDC_PARAM_RADIANS               1002
-#define IDC_COMBO1                      1003
-#define IDC_MFCCOLORBUTTON1             1004
-#define IDC_SLIDER2                     1007
-#define IDC_CUSTOM1                     1009
-#define IDC_SPLIT1                      1010
 #define IDC_PARAM_NAME                  1014
 #define IDC_PARAM_T_REAL                1015
 #define IDC_PARAM_T_COMPLEX             1016
@@ -44,16 +25,13 @@
 #define IDC_PARAM_IMAX                  1023
 #define IDC_PARAM_ABSMAX                1024
 #define IDC_DEF_FORMULA                 1025
-#define ID_FILE_EXIT                    32776
 #define ID_VIEW_PROPERTIES              32777
-#define ID_ACCELERATOR32778             32778
-#define ID_ACCELERATOR32779             32779
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        319
+#define _APS_NEXT_RESOURCE_VALUE        320
 #define _APS_NEXT_COMMAND_VALUE         32781
 #define _APS_NEXT_CONTROL_VALUE         1026
 #define _APS_NEXT_SYMED_VALUE           310
