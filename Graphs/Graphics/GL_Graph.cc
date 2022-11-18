f4a64911d6c7c6b34ab94eeab5349db122bef640
Torsten <th@zoon.cc>
2022-11-18 12:55:03 +0100

    Fix combined axis+custom clipping

--- Graphs/Graphics/GL_Graph.cc
+++ Graphs/Graphics/GL_Graph.cc
@@ -4,34 +4,48 @@
 #include "../Geometry/Axis.h"
 #include "../OpenGL/GL_Context.h"
 
-static inline bool clip(const Graph &graph)
+static inline int max_planes()
 {
-	return !graph.plot.options.clip.on() && graph.clipping();
+	static int N = 0;
+	if (!N) glGetIntegerv(GL_MAX_CLIP_PLANES, &N);
+	assert(N >= 6); // 6 should be minimum
+	return N;
+}
+
+static inline bool clip(const Graph &graph, int &first_plane_id)
+{
+	if (!graph.clipping()) return false;
+	bool ccp = graph.plot.options.clip.on();
+	if (ccp && max_planes() <= 6) return false;
+	first_plane_id = GL_CLIP_PLANE0 + ccp;
+	return true;
 }
 
 void GL_Graph::start_drawing() const
 {
 	GL_CHECK;
 	
+	int P = -1;
 	const Axis &axis = graph.plot.axis;
-	if (axis.type() == Axis::Box && clip(graph))
+	if (axis.type() == Axis::Box && clip(graph, P))
 	{
+		assert(P >= GL_CLIP_PLANE0);
 		P4d cf;
 		double r;
 
 		r = fabs(axis.range(2) / axis.range(0)) + 1e-4;
-		cf.set(0, 0, -1, r); glClipPlane(GL_CLIP_PLANE0, cf); glEnable(GL_CLIP_PLANE0);
-		cf.set(0, 0,  1, r); glClipPlane(GL_CLIP_PLANE1, cf); glEnable(GL_CLIP_PLANE1);
+		cf.set(0, 0, -1, r); glClipPlane(P, cf); glEnable(P++);
+		cf.set(0, 0,  1, r); glClipPlane(P, cf); glEnable(P++);
 
 		if (!graph.isGraph())
 		{
 			r = 1.0 + 1e-4; // +1e-4 saves the outermost grid lines of a graph from being clipped
-			cf.set(-1, 0, 0, r); glClipPlane(GL_CLIP_PLANE2, cf); glEnable(GL_CLIP_PLANE2);
-			cf.set( 1, 0, 0, r); glClipPlane(GL_CLIP_PLANE3, cf); glEnable(GL_CLIP_PLANE3);
+			cf.set(-1, 0, 0, r); glClipPlane(P, cf); glEnable(P++);
+			cf.set( 1, 0, 0, r); glClipPlane(P, cf); glEnable(P++);
 			
 			r = fabs(axis.range(1) / axis.range(0)) + 1e-4;
-			cf.set(0, -1, 0, r); glClipPlane(GL_CLIP_PLANE4, cf); glEnable(GL_CLIP_PLANE4);
-			cf.set(0,  1, 0, r); glClipPlane(GL_CLIP_PLANE5, cf); glEnable(GL_CLIP_PLANE5);
+			cf.set(0, -1, 0, r); glClipPlane(P, cf); glEnable(P++);
+			cf.set(0,  1, 0, r); glClipPlane(P, cf); glEnable(P++);
 		}
 	}
 
@@ -42,14 +56,11 @@ void GL_Graph::finish_drawing() const
 {
 	GL_CHECK;
 
-	if (graph.plot.axis.type() == Axis::Box && clip(graph))
+	int P = -1;
+	if (graph.plot.axis.type() == Axis::Box && clip(graph, P))
 	{
-		glDisable(GL_CLIP_PLANE0);
-		glDisable(GL_CLIP_PLANE1);
-		glDisable(GL_CLIP_PLANE2);
-		glDisable(GL_CLIP_PLANE3);
-		glDisable(GL_CLIP_PLANE4);
-		glDisable(GL_CLIP_PLANE5);
+		assert(P >= GL_CLIP_PLANE0);
+		for (int i = 0; i < 6; ++i) glDisable(P+i);
 	}
 
 	glDepthMask(GL_TRUE);
